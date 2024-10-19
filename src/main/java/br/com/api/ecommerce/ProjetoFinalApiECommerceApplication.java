d573103a40cf00a5e153366caec4490df388e90f
rafasalmeron <rafasalmeronjr@gmail.com>
2024-10-19 15:19:07 -0300

    Criação do Projeto e suas depêndencias.

---
+++ src/main/java/br/com/api/ecommerce/ProjetoFinalApiECommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApiECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFinalApiECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/api/ecommerce/ProjetoFinalApiECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApiECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
