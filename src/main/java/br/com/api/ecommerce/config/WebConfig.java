d8ebe52d401e901ec3a230072480984db7a57a1b
Rafael Salmeron <rafasalmeronjr@gmail.com>
2024-11-05 17:56:59 -0300

    Update WebConfig.java

--- src/main/java/br/com/api/ecommerce/config/WebConfig.java
+++ src/main/java/br/com/api/ecommerce/config/WebConfig.java
@@ -10,11 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-
-                .allowedOrigins("http://localhost:5173/")
-
-                .allowedOrigins("https://front-end-projeto-api-e-commerce-pveh9ccvn.vercel.app/")
-
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
